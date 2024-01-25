789bcc78547af2233ad446e23100ea52fb96a66e
mrAbdhas <143132916+mrAbdhas@users.noreply.github.com>
2024-01-25 21:12:36 +0100

    Step1-20

---
+++ script.js
@@ -0,0 +1,162 @@
+//Accessing html elements with getElementBuId() and assigning them to variables
+const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+const shuffleButton = document.getElementById("shuffle");
+
+/* Array to store all the songs,
+the array holds an object with properties and values */
+const allSongs = [
+    {
+        id: 0,
+        title: "Scratching The Surface",
+        artist: "Quincy Larson",
+        duration: "4:25",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3",
+    },
+    {
+        id: 1,
+        title: "Can't Stay Down",
+        artist: "Quincy Larson",
+        duration: "4:15",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3",
+    },
+    {
+        id: 2,
+        title: "Still Learning",
+        artist: "Quincy Larson",
+        duration: "3:51",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/still-learning.mp3",
+        
+    },
+    {
+        id: 3,
+        title: "Cruising for a Musing",
+        artist: "Quincy Larson",
+        duration: "3:34",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cruising-for-a-musing.mp3",
+      },
+      {
+        id: 4,
+        title: "Never Not Favored",
+        artist: "Quincy Larson",
+        duration: "3:35",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/never-not-favored.mp3",
+      },
+      {
+        id: 5,
+        title: "From the Ground Up",
+        artist: "Quincy Larson",
+        duration: "3:12",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/from-the-ground-up.mp3",
+      },
+      {
+        id: 6,
+        title: "Walking on Air",
+        artist: "Quincy Larson",
+        duration: "3:25",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/walking-on-air.mp3",
+      },
+      {
+        id: 7,
+        title: "Can't Stop Me. Can't Even Slow Me Down.",
+        artist: "Quincy Larson",
+        duration: "3:52",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stop-me-cant-even-slow-me-down.mp3",
+      },
+      {
+        id: 8,
+        title: "The Surest Way Out is Through",
+        artist: "Quincy Larson",
+        duration: "3:10",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/the-surest-way-out-is-through.mp3",
+      },
+      {
+        id: 9,
+        title: "Chasing That Feeling",
+        artist: "Quincy Larson",
+        duration: "2:43",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/chasing-that-feeling.mp3",
+      },
+
+
+];
+/* new Audio(). This will create a new HTML5 audio element. */
+const audio = new Audio();
+
+
+/* a userData object that will contain the songs, 
+the current song playing, and the time of the current song. */
+
+/* users will be able to shuffle and delete 
+songs from the playlist, you will need to 
+create a copy of the allSongs array without
+mutating the original. This is where the
+spread operator comes in handy.
+The spread operator (...) allows you to
+copy all elements from one array into
+another. It can also be used to
+concatenate multiple arrays into one. */
+let userData = {
+  songs: [...allSongs],
+  currentSong: null,
+  songCurrentTime: 0,
+};
+//
+
+/* to display the songs in the UI. To do this,
+you'll create a renderSongs function using the
+arrow function syntax */
+// Arrow function
+const renderSongs = (array) => {
+
+  /* The map() method is used to iterate through
+  an array and return a new array. It's helpful
+  when you want to create a new array based on
+  the values of an existing array. */
+  const songsHTML = array.map((song) => {
+
+ return `
+      <li id="song-${song.id}" class="playlist-song">
+      <button class="playlist-song-info">
+          <span class="playlist-song-title">${song.title}</span>
+          <span class="playlist-song-artist">${song.artist}</span>
+          <span class="playlist-song-duration">${song.duration}</span>
+      </button>
+      <button class="playlist-song-delete" aria-label="Delete ${song.title}">
+          <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
+        </button>
+      </li>
+      `;
+    /* the join() method to combine the songsHTML markup into a single string. */
+    } ).join("");
+  //
+  //
+  /*update the playlist in your HTML document
+  to display the songs. Assign songsHTML to 
+  the innerHTML property of the playlistSongs
+  element. This will insert the li element 
+  you just created into the ul element in 
+  the already provided HTML file. */
+    playlistSongs.innerHTML = songsHTML;
+  //
+};
+
+
+/* when you call the renderSongs function,
+you'll pass in the songs array inside the 
+userData object. When you do, the function 
+will loop through the array and build HTML for all the songs */
+/*Now you need to call the renderSongs function
+and pass in userData?.songs in order to finally
+display the songs in the UI. */
+
+
+renderSongs(userData?.songs);
+/* Optional chaining (?.) helps prevent errors
+when accessing nested properties that might
+be null or undefined. */
+//
